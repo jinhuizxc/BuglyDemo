3d20ee1b8912844adca720895f6fec15e8c03ddd
j_h <13113017500>
2019-07-29 18:50:04 +0800

    空指针异常上报测试;

--- app/src/main/java/com/example/buglydemo/MainActivity.java
+++ app/src/main/java/com/example/buglydemo/MainActivity.java
@@ -1,16 +1,64 @@
 package com.example.buglydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 测试下bugly, 出现异常可以上报好排查问题！
  */
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.button1)
+    Button button1;
+//    @BindView(R.id.button2)
+    Button button2;
+    Button button3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "crash", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "crash3", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @OnClick({R.id.button, R.id.button1})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+//                CrashReport.testJavaCrash();
+                break;
+            case R.id.button1:
+                Toast.makeText(this, "可点击", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
