98bdac3256a8e322df22ce251755f25b7eda64ef
j_h <13113017500>
2019-07-29 17:18:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/buglydemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.buglydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 测试下bugly, 出现异常可以上报好排查问题！
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
